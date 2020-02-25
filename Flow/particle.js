3c62c9edeec767cd51f546c43de66fb3c17487d9
hugopeeters <hpeeters@me.com>
2020-02-25 10:56:27 +0100

    tweaked flow params

--- Flow/particle.js
+++ Flow/particle.js
@@ -1,47 +1,47 @@
 class Particle {
 
-  constructor(pos) {
-    this.pos = pos;
-    this.ppos = pos.copy();
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.radius = 2;
-  }
+    constructor(pos) {
+        this.pos = pos;
+        this.ppos = pos.copy();
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
+        this.radius = 2;
+    }
 
-  render() {
-    //noStroke();
-    //fill(frameCount / 10 % 255, 255, 255, 100);
-    //ellipse(pos.x, pos.y, radius, radius);
-    strokeWeight(1);
-    stroke(frameCount / 10 % 255, 255, 255, lineOpacity);
-    //stroke(random(25), 255, 255, lineOpacity);
-    line(this.ppos.x, this.ppos.y, this.pos.x, this.pos.y);
-  }
+    render() {
+        //noStroke();
+        //fill(frameCount / 10 % 255, 255, 255, 100);
+        //ellipse(pos.x, pos.y, radius, radius);
+        strokeWeight(1);
+        stroke(frameCount / 10 % 255, 255, 255, lineOpacity);
+        //stroke(random(25), 255, 255, lineOpacity);
+        line(this.ppos.x, this.ppos.y, this.pos.x, this.pos.y);
+    }
 
- update() {
-    let force = flowfield[round(this.pos.x/fieldResolution)][round(this.pos.y/fieldResolution)];
-    force.setMag(fieldForce);
-    this.acc.add(force);
-    this.vel.add(this.acc);
-    this.vel.limit(maxVel);
-    this.ppos = this.pos.copy();
-    this.pos.add(this.vel);
-    if (this.pos.x <= 0) { 
-      this.pos.x += width;
-      this.ppos.x += width;
+    update() {
+        let force = flowfield[round(this.pos.x / fieldResolution)][round(this.pos.y / fieldResolution)];
+        force.setMag(fieldForce);
+        this.acc.add(force);
+        this.vel.add(this.acc);
+        this.vel.limit(maxVel);
+        this.ppos = this.pos.copy();
+        this.pos.add(this.vel);
+        if (this.pos.x <= -10) {
+            this.pos.x += width + 20;
+            this.ppos.x += width + 20;
+        }
+        if (this.pos.x >= width + 10) {
+            this.pos.x -= width + 20;
+            this.ppos.x -= width + 20;
+        }
+        if (this.pos.y <= -10) {
+            this.pos.y += height + 20;
+            this.ppos.y += height + 20;
+        }
+        if (this.pos.y >= height + 10) {
+            this.pos.y -= height + 20;
+            this.ppos.y -= height + 20;
+        }
+        this.acc.setMag(0);
     }
-    if (this.pos.x >= width) { 
-      this.pos.x -= width;
-      this.ppos.x -= width;
-    }
-    if (this.pos.y <= 0) { 
-      this.pos.y += height;
-      this.ppos.y += height;
-    }
-    if (this.pos.y >= height) { 
-      this.pos.y -= height;
-      this.ppos.y -= height;
-    }
-    this.acc.setMag(0);
-  }
 }
--- Flow/sketch.js
+++ Flow/sketch.js
@@ -1,5 +1,5 @@
-let fieldResolution = 10;
-let fieldScale = 0.01;
+let fieldResolution = 20;
+let fieldScale = 0.05;
 let changeRate = 0.00005;
 let numP = 5000;
 let lineOpacity = 0.075;
@@ -13,47 +13,47 @@ let zoff = 0;
 let cols, rows, particles;
 
 function setup() {
-  createP().parent('canvas');
-  let canvas = createCanvas(800, 600);
-  canvas.parent('canvas');
-  createP().parent('canvas');
-  cols = floor(width/fieldResolution);
-  rows = floor(height/fieldResolution);
-  colorMode(HSB, 255, 255, 255);
-  rectMode(CENTER);
-  ellipseMode(CENTER);
-  particles = new Array(numP);
-  for (let i = 0; i < numP; i++) {
-    particles[i] = new Particle(createVector(random(width), random(height)));
-  }
-  background(0);
+    createP().parent('canvas');
+    let canvas = createCanvas(800, 600);
+    canvas.parent('canvas');
+    createP().parent('canvas');
+    cols = floor(width / fieldResolution + 2);
+    rows = floor(height / fieldResolution + 2);
+    colorMode(HSB, 255, 255, 255);
+    rectMode(CENTER);
+    ellipseMode(CENTER);
+    particles = new Array(numP);
+    for (let i = 0; i < numP; i++) {
+        particles[i] = new Particle(createVector(random(width), random(height)));
+    }
+    background(0);
 }
 
 function draw() {
-  //background(0);
+    //background(0);
 
-  for (let x = 0; x < cols + 1; x += 1) {
-    flowfield[x] = [];
-    for (let y = 0; y < rows + 1; y += 1) {
-      xoff = x * fieldScale;
-      yoff = y * fieldScale;
-      flowfield[x][y] = p5.Vector.fromAngle(noise(xoff, yoff, zoff) * TWO_PI);
-      
-      if (showVectors) {
-        //DRAW VECTORS
-        push();
-        translate(x * fieldResolution, y * fieldResolution);
-        rotate(flowfield[x][y].heading());
-        stroke(255, 0.01);
-        line(0, 0, fieldResolution*0.75, 0);
-        pop();
-      }
+    for (let x = 0; x < cols + 1; x += 1) {
+        flowfield[x] = [];
+        for (let y = 0; y < rows + 1; y += 1) {
+            xoff = x * fieldScale;
+            yoff = y * fieldScale;
+            flowfield[x][y] = p5.Vector.fromAngle(noise(xoff, yoff, zoff) * TWO_PI);
+
+            if (showVectors) {
+                //DRAW VECTORS
+                push();
+                translate(x * fieldResolution, y * fieldResolution);
+                rotate(flowfield[x][y].heading());
+                stroke(255, 0.01);
+                line(0, 0, fieldResolution * 0.75, 0);
+                pop();
+            }
+        }
     }
-  }
-  zoff += changeRate;
+    zoff += changeRate;
 
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].render();
-    particles[i].update();
-  }
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].render();
+        particles[i].update();
+    }
 }
