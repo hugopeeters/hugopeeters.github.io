83e59ec1537dc7710ce898ffafe29d365ea5875f
hugopeeters <hpeeters@me.com>
2020-02-26 20:56:05 +0100

    First playable prototype

---
+++ Beats/grid.js
@@ -0,0 +1,55 @@
+class Grid {
+
+    constructor() {
+        this.cells = new Array(4 * numBars);
+        for (let i = 0; i < this.cells.length; i++) {
+            this.cells[i] = new Array(soundNames.length);
+            for (let j = 0; j < this.cells[i].length; j++) {
+                this.cells[i][j] = false;
+            }
+        }
+        this.cellWidth = (width - leftMargin) / this.cells.length;
+        this.cellHeight = height / soundNames.length;
+        //console.log(this.cells);
+    }
+
+    draw() {
+
+        //draw cells
+        for (let i = 0; i < this.cells.length; i++) {
+            for (let j = 0; j < this.cells[i].length; j++) {
+                let x = leftMargin + i * this.cellWidth;
+                let y = j * this.cellHeight;
+                if (this.cells[i][j]) {
+                    fill(255, 0, 0);
+                } else {
+                    noFill();
+                }
+                stroke(255);
+                rect(x, y, this.cellWidth, this.cellHeight);
+            }
+        }
+        //draw labels
+        for (let j = 0; j < this.cells[0].length; j++) {
+            let x = 10;
+            let y = (j + 0.6) * this.cellHeight;
+            noStroke();
+            fill(255);
+            text(soundNames[j], x, y);
+        }
+    }
+
+    toggle(col, row) {
+        this.cells[col][row] = !this.cells[col][row];
+    }
+
+    play(_count) {
+        let count = _count % (numBars * 4);
+        for (let j = 0; j < this.cells[count].length; j++) {
+            if (this.cells[count][j]) {
+                playSound(soundNames[j]);
+            }
+        }
+    }
+
+}
--- Beats/sketch.js
+++ Beats/sketch.js
@@ -1,31 +1,52 @@
 let soundNames = ["cl_hihat", "claves", "conga1", "cowbell", "crashcym", "handclap", "hi_conga", "hightom", "kick1", "kick2", "maracas", "open_hh", "rimshot", "snare", "tom1"];
+let numBars = 4;
+let grid;
+let leftMargin = 75;
+let playLoop = false;
+let speed = 10; //one count is this many frames
+let startFrame;
 
 function setup() {
     createP().parent('canvas');
-    let canvas = createCanvas(800, 600);
+    let canvas = createCanvas(800, 800);
     canvas.parent('canvas');
     createP().parent('canvas');
     for (let soundName of soundNames) {
         loadSound(soundName);
     }
+    grid = new Grid();
+    frameRate(100);
 }
 
 function draw() {
     background(51);
+    grid.draw();
+    if(playLoop && (frameCount - startFrame) % speed == 0) {
+        console.log("playing something");
+        grid.play(floor((frameCount - startFrame) / speed));
+    }
 }
 
 function mouseClicked() {
-    //playSound(random(soundNames));
+    let col = floor((mouseX - leftMargin) / grid.cellWidth);
+    let row = floor(mouseY / grid.cellHeight);
+    //console.log("r " + row + " c " + col); 
+    playSound(soundNames[row]);
+    grid.toggle(col, row);
+}
+
+function keyPressed(){
+    playLoop = !playLoop;
+    startFrame = frameCount;
+    console.log("Playing: " + playLoop);
 }
 
 function loadSound(soundID) {
     createjs.Sound.registerSound("sounds/" + soundID + ".mp3", soundID);
-    console.log("sound " + soundID + " registered");
-
+    //console.log("sound " + soundID + " registered");
 }
 
 function playSound(soundID) {
     createjs.Sound.play(soundID);
-    console.log("sound " + soundID + " played?");
-
+    //console.log("sound " + soundID + " played");
 }
