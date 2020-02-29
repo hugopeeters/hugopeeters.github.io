e2b68dd5355fcf3909b57eb32d99a4e7f57809a4
hugopeeters <hpeeters@me.com>
2020-02-29 16:36:36 +0100

    Vertical wipe with random red

--- Vibes/sketch.js
+++ Vibes/sketch.js
@@ -1,7 +1,9 @@
 let logo;
+let y = 0;
+let red;
 
 function preload() {
-    logo = loadImage('logo/Eindhoven_ZW_Beeldmerk_BB.png');
+    logo = loadImage('logo/logo.png');
 }
 
 function setup() {
@@ -9,14 +11,37 @@ function setup() {
     let canvas = createCanvas(800, 800);
     canvas.parent('canvas');
     createP().parent('canvas');
+    red = floor(random(0, 255));
+    console.log(red);
+    
 }
 
 function draw() {
     background(51);
-    image(logo, 0, 0, width, height);
-    let x = width / 4;
-    let y = height / 2;
-    fill(255);
-    textSize(64);
-    text('Coming Soon', x, y);
+    logo.loadPixels();
+    let d = pixelDensity();
+    for (let x = 0; x < width; x++) {
+        let off = (y * width + x) * d * 4;
+        
+        if (logo.pixels[off + 3] == 255) {
+            for (let i = 0; i < d; i++) {
+                logo.pixels[off + i] = red;
+                logo.pixels[off + i + 1] = 0;
+                logo.pixels[off + i + 2] = 0;
+                logo.pixels[off + i + 3] = red;
+            }
+        }
+    }
+    logo.updatePixels();
+
+    //draw the image
+    image(logo, 0, 0);
+
+    y++;
+    if (y >= height) {
+        y = 0;
+        red = floor(random(0, 255));
+        console.log(red);
+        preload();
+    }
 }
